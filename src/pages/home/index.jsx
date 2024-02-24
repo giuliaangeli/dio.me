9489ae4fac1851f436633fbf1a7fb6176141a12d
Giulia Angeli <novodono@MacBook-Air-de-Novo.local>
2024-02-24 09:55:03 -0300

    create home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,35 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import bannerImage from "../../assets/banner.png";
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Container, TextContent, Title, TitleHighlight } from './style';
+
 const Home = () => {
+    const navigate = useNavigate();
+    const handleClickSignIn = () => {
+        navigate('/login')
+    }
+    
     return (
         <>
-        <h1>Home</h1>
-        <Link to="/login">Fazer login</Link>
+        <Header/>
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente <br/>
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title={"Começar agora"} variant='secundary' onClick={handleClickSignIn}/>
+            </div>
+            <div>
+                <img src={bannerImage} alt="Imagem Banner"/>
+            </div>
+        </Container>
         </>
     )
 }
